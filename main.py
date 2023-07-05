0ddf0eeb7b039c76d6749d2943faf2c58c4fc501
Jayanth-Mani <69757920+Jayanth-Mani@users.noreply.github.com>
2023-07-05 11:16:11 -0400

    Update to use numpy arrays

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import ctypes
+import numpy as np
 
 # add path to so file
 clibrary = ctypes.CDLL(
@@ -18,8 +19,10 @@ values2 = [2, 4, 6, 11, 19, 21, 23, 24, 11, 13]
 array = (ctypes.c_int * len(values))(*values)
 array2 = (ctypes.c_int * len(values2))(*values2)
 test = array_add(array, array2, len(values))
-for i in range(len(values)):
-    print(test[i], end=", ")
+# test_value = np.fromiter(test, dtype=np.int32, count=len(values))
+address = ctypes.addressof(test.contents)
+test_value = np.frombuffer((ctypes.c_int * len(values)).from_address(address), int)
+print(test_value)
 free_mem = clibrary.free_memory
 free_mem.arg_types = [ctypes.c_void_p]
 free_mem(test)
